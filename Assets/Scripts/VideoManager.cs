ecc862b3f864247a64e59124cace728d85caddd6
stevenjmarsh <stevenjmarsh@users.noreply.github.com>
2019-08-07 18:41:42 -0400

    add play button and stop button, wired to video player

--- Assets/Scripts/VideoManager.cs
+++ Assets/Scripts/VideoManager.cs
@@ -1,17 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class VideoManager : MonoBehaviour
 {
+  private VideoPlayer videoPlayer;
+
   // Start is called before the first frame update
   void Start()
   {
-
+    videoPlayer = GetComponent<VideoPlayer>();
+    if (!videoPlayer)
+    {
+      Debug.Log("(SJM) VideoPlayer component NOT found!!");
+    }
   }
 
   public void StreamVideoUrl(string url)
   {
-    Debug.Log("stream video:" + url);
+    videoPlayer.url = url;
+    videoPlayer.Play();
+  }
+
+  public void StopVideo()
+  {
+    videoPlayer.Stop();
   }
 }
